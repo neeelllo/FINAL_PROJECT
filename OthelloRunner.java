f7bb1bfac8d660054f39595fc1cd9883ca50c851
david <david.corona1029307@gmail.com>
2017-05-09 11:46:10 -0700

    hi

--- OthelloRunner.java
+++ OthelloRunner.java
@@ -2,7 +2,7 @@ import java.awt.Color;          // to change frame color
 import java.awt.event.*;        // actionlistnenters for the buttons
 import java.awt.*;              // for layout, insets
 import javax.swing.ImageIcon;   // change image icon of buttons
-import javax.swing.JButton;     // JFrame UI stuff
+import javax.swing.JButton;     // JFrame UI
 import javax.swing.JFrame;
 import javax.swing.JPanel;  
 import java.io.*;   //file reading
@@ -21,12 +21,34 @@ public class OthelloRunner extends JFrame
     static ImageIcon redCircle = new ImageIcon("images/redCircle.jpg", "Red");
     static ImageIcon blackCircle = new ImageIcon("images/blackCircle.jpg", "Black");
     static ImageIcon noCircle = new ImageIcon("images/noCircle.jpg", "No");
+
+    int [][] board = new int [8][8];
+    boolean gridFull = false;
+
     // constructors
     OthelloRunner()
     {
-        
+
     }//Ends OthelloRunner() constructor
     // methods
+    void checkGrid()
+    {
+        for(int row = 0; row <= board.length; row++)
+        {
+            for(int col = 0; col <= board.length; col++)
+            {
+                if(board[row][col] == 0)
+                {
+                    gridFull = false;
+                }
+                else
+                {
+                    gridFull = true;
+                }
+            }
+        }
+    }
+
     /**
      * Sandwich - Scans the grid for all possible affected pieces and changes 
      * them if all apply to the requirments
@@ -34,8 +56,18 @@ public class OthelloRunner extends JFrame
      * @param - recieves the coordinates of the placed piece
      * @return - changes all affected pieces
      */
-    public void sandwich()
+    public void sandwich(int x, int y)
     {
-        
+        while(gridFull == false)
+        {
+            for(int c = -1; c <= 1; c++)
+            {
+                for(int r = -1; r <= 1; r++)
+                {
+                    if(board[x + c][y + r] == 0)
+                    {}
+                }
+            }
+        }
     }//Ends () method
 }// ends OthelloRunner class

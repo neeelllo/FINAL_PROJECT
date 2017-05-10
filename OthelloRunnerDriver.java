982a92b79f4e756de9513c4a1f103142883d3c7d
Leo <neil123lerios@live.com>
2017-05-10 11:17:18 -0700

    here u go bby

--- OthelloRunnerDriver.java
+++ OthelloRunnerDriver.java
@@ -2,8 +2,7 @@ class OthelloRunnerDriver
 {
     public static void main(String[] args)
     {
-        OthelloRunner in = new OthelloRunner();
-        int x = 0, y = 0;
-        in.sandwich(x, y);
+        OthelloRunner game = new OthelloRunner();
+        game.start();
     }
 }
